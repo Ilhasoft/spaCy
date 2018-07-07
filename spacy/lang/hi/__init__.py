df908f9f88d43a93d29d9e253211146acae9d136
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-07-07 22:12:39 +0000

    Add Hindi language files

--- spacy/lang/hi/__init__.py
+++ spacy/lang/hi/__init__.py
@@ -1,24 +1,31 @@
 # coding: utf8
 from __future__ import unicode_literals
 
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .stop_words import STOP_WORDS
-from .lex_attrs import LEX_ATTRS
+from .norm_exceptions import NORM_EXCEPTIONS
+from .morph_rules import MORPH_RULES
+from .lemmatizer import LOOKUP
 
+from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ..norm_exceptions import BASE_NORMS
 from ...language import Language
-from ...attrs import LANG
+from ...attrs import LANG, NORM
+from ...util import update_exc, add_lookups
 
 
 class HindiDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
-    lex_attr_getters.update(LEX_ATTRS)
     lex_attr_getters[LANG] = lambda text: 'hi'
-    stop_words = STOP_WORDS
+    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM], BASE_NORMS)
+
+    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
+    stop_words = set(STOP_WORDS)
+    lemma_lookup = LOOKUP
 
 
 class Hindi(Language):
     lang = 'hi'
     Defaults = HindiDefaults
-
 
 __all__ = ['Hindi']
